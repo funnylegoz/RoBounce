7cc65a0560ca69bd9e33035cb5eee62d51b06368
funnylegoz <166183716+funnylegoz@users.noreply.github.com>
2024-04-06 22:06:35 +0100

    Update MainScript.lua

--- Main/MainScript.lua
+++ Main/MainScript.lua
@@ -1,15 +1,2 @@
--- Latest Script Regarding RoBounce/liquidbounceNightly
-
-local function sendNotification(message)
-    local player = game.Players.LocalPlayer
-    local Notification = Instance.new("Message")
-    Notification.Text = message
-    Notification.Parent = player:WaitForChild("PlayerGui")
-    wait(5)
-    Notification:Destroy()
-end
-
-sendNotification("RoBounce is now running in this server, please press ; to open the UI")
- wait(3)
- print("RoBounce has notified the player it is avaliable to use.")
-end)
+-- Importing Source
+local RoBounceModules = loadstring(game:HttpGet("NOTDONEYET", true))()
