58da5aa09691bda961bd35e1ad9aca205ec4c941
funnylegoz <166183716+funnylegoz@users.noreply.github.com>
2024-04-07 23:41:09 +0100

    Create installertest.lua

---
+++ Main/installertest.lua
@@ -0,0 +1,7 @@
+makefolder(Workspace/RoBounce)
+ writefile(Workspace/RoBounce/TEST.lua, print("ROBOUNCETEST"))  
+
+--[[This should make a file in workspace that prints ROBOUNCETEST, t
+his is for future updates lol, this will NOT be implemented into RoBounce js yet as I am not
+ready for it.
+]]
