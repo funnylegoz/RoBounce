79dbcc1a39c9bae136fb4ccdd7006c8db5f34694
funnylegoz <166183716+funnylegoz@users.noreply.github.com>
2024-04-07 04:47:23 +0100

    Update MainScript.lua
    Using new library and preserving it.

--- Main/MainScript.lua
+++ Main/MainScript.lua
@@ -1,80 +1,32 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local RoBounceModules = loadstring(game:HttpGet("https://raw.githubusercontent.com/funnylegoz/RoBounce/main/Main/RoBounceModulesLibrary.lua"))()
+local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt")()
 
-local Window = Fluent:CreateWindow({
-    Title = "RoBounce",
-    SubTitle = "By RoBounce Development",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
-    Acrylic = false,
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl
-})
+local win = lib:Window("PREVIEW",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
 
---Uses lucide.dev icons, tab icon usage is optional 
-local Tabs = {
-    Main = Window:AddTab({ Title = "Main", Icon = "box" }),
-    Settings = Window:AddTab({ Title = "Settings", Icon = "settings-2" })
-}
+local tab = win:Tab("Main")
 
-    local Keybind = Tabs.Main:AddKeybind("Keybind", {
-        Title = "KeyBind",
-        Mode = "Toggle",
-        Default = "LeftControl",
+tab:Button("InfYield", function()
+lib:Notification("Notification", "Hello!", "Hi!")
+end)
 
- 
-        Callback = function(Value)
-            print("Keybind clicked!", Value)
-        end,
+tab:Toggle("Toggle",false, function(t)
+print(t)
+end)
+
+tab:Slider("Slider",0,100,30, function(t)
+print(t)
+end)
+
+tab:Dropdown("Dropdown",{"Option 1","Option 2","Option 3","Option 4","Option 5"}, function(t)
+print(t)
+end)
 
 
-        ChangedCallback = function(New)
-            print("Keybind changed!", New)
-        end
-    })
+tab:Textbox("Textbox",true, function(t)
+print(t)
+end)
 
-    -- OnClick is only fired when you press the keybind and the mode is Toggle
-    -- Otherwise, you will have to use Keybind:GetState()
-    Keybind:OnClick(function()
-        print("Keybind clicked:", Keybind:GetState())
-    end)
+tab:Bind("Bind",Enum.KeyCode.RightShift, function()
+print("RoBounce Keybind Press")
+end)
 
-    Keybind:OnChanged(function()
-Fluent:Notify({
-    Title = "RoBounce",
-    Content = "You have changed your RoBounce Keybind!",
-    Duration = 3
-})
-    end)
-
-    task.spawn(function()
-        while true do
-            wait(1)
-
-
-            local state = Keybind:GetState()
-            if state then
-                print("funny hahas")
-            end
-
-            if Fluent.Unloaded then break end
-        end
-    end)
--- Main Area
-
- local Toggle = Tabs.Main:AddToggle("Tracer Toggle", {Title = "Tracers", Default = false })
-
-    Toggle:OnChanged(function()
-RoBounce.TracersModule()
-    end)
-
-    Options.MyToggle:SetValue(false)
-
-
-Window:SelectTab(1)
-
-Fluent:Notify({
-    Title = "RoBounce",
-    Content = "RoBounce has been initialized, please press LCTRL to open the UI.",
-    Duration = 8
-})
+tab:Label("Label")
