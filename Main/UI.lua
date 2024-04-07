5e779ee31ff2b464b56d1ac3231c7cca34e67bab
funnylegoz <166183716+funnylegoz@users.noreply.github.com>
2024-04-07 02:10:43 +0100

    Create UI.lua

---
+++ Main/UI.lua
@@ -0,0 +1,11 @@
+local RoBounceUIVariablesTable = {}
+local userInterface = game:GetService(“UserInputService”)
+local starterGUI = game:GetService(“StarterGui”)
+local lplr = game.Players.LocalPlayer
+local runService = game:GetService("RunService")
+local inputService = game:GetService("UserInputService")
+local httpService = game:GetService("HttpService")
+local tweenService = game:GetService("TweenService")
+local guiService = game:GetService("GuiService")
+local textService = game:GetService("TextService")
+return RoBounceUIVariablesTable
