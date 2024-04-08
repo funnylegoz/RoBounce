5eadf2ab7943e2f41dca90c33771c47aaf41f032
funnylegoz <166183716+funnylegoz@users.noreply.github.com>
2024-04-08 01:51:25 +0100

    Delete Main/installertest.lua

--- Main/installertest.lua
+++
@@ -1,7 +0,0 @@
-makefolder(Workspace/RoBounce)
- writefile(Workspace/RoBounce/TEST.lua, print("ROBOUNCETEST"))  
-
---[[This should make a file in workspace that prints ROBOUNCETEST, t
-his is for future updates lol, this will NOT be implemented into RoBounce js yet as I am not
-ready for it.
-]]
