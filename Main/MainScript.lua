c74a181c7308e70150e410db20279c5bfa07a7b6
funnylegoz <166183716+funnylegoz@users.noreply.github.com>
2024-04-08 01:59:14 +0100

    Update MainScript.lua

--- Main/MainScript.lua
+++ Main/MainScript.lua
@@ -1,22 +1,9 @@
-local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/funnylegoz/RoBounce/main/Main/UILIBRARY.lua")()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Torrize/Roblox-Exploiting-UI-Library/main/source.lua"))()
 
-local win = lib:Window("RoBounce Public Beta Testing 0.01b",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
+local MainWindow = Library:CreateWindow(RBTEST) 
 
-local tab = win:Tab("Universal")
+local NewTab = MainWindow:NewTab(Main)
 
-local tab2 = win:Tab("Bedwars")
-
-tab:Button("InfYield", function()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-lib:Notification("Notification", "Loaded Infinite Yield", ">>")
-end)
-
-tab:Button("OwlHub", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
-lib:Notification("Notification", "Loaded OwlHub", ">>")
-end)
-
-tab2:Button("SafeSpeed", function()
-game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = 23
-lib:Notification("Notification", "Applied SafeSpeed", ">>")
+local NewTextbox = NewTab:NewTextbox(Textbox, Type something, function(Text)
+    print(Text)
 end)
